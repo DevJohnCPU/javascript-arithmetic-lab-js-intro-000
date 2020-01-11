17913c208d7c4d760ed6bf33855df5c50b14679c
John <johnps2010@hotmail.com>
2020-01-11 17:29:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,10 +25,10 @@ function dec(n){
 }
 
 function makeInt(n){
-   parseInt( n , 10 )
+   parseInt( 'n' , 10 )
    return n;
 }
 
 function preserveDecimal(n){
-  parseFloat( n )
+  parseFloat( 'n' )
 }
